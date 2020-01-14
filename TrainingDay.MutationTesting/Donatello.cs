ba130ebedf7cde2e98e8296126cc670cf8a34b94
Ion Dormenco <idormenco@gmail.com>
2020-01-14 13:28:55 +0200

    add string mutations example

--- TrainingDay.MutationTesting/Donatello.cs
+++ TrainingDay.MutationTesting/Donatello.cs
@@ -1,21 +1,43 @@
 ﻿// ReSharper disable All
+using System;
+
 namespace TrainingDay.MutationTesting
 {
     public class Donatello
     {
         private const decimal DamageRezist = 0.1m;
         public decimal HealthPoints { get; private set; } = 100;
-        
-        public void IaBataie(decimal damage)
+
+        private bool ApplyDamage(decimal damage)
         {
+            if (damage < 0)
+                throw new ArgumentException("Invalid something something , bad bad !", nameof(damage));
+
+
             decimal damageToTake = damage;
 
-            if(damage >= 50)
+            if (damage >= 50)
             {
                 damageToTake = damage - (damage * DamageRezist);
             }
 
             HealthPoints = HealthPoints - damageToTake;
+
+            return true;
+        }
+
+        public string IaBataie(decimal damage)
+        {
+            try
+            {
+                if (ApplyDamage(damage)) return "Successfully";
+                // do more stuff
+            }
+            catch (ArgumentException e)
+            {
+                throw new Exception("Something happened", e);
+            }
+            return null;
         }
     }
 }
--- TrainingDay.Tests/DonatelloTests.cs
+++ TrainingDay.Tests/DonatelloTests.cs
@@ -1,4 +1,5 @@
-﻿using Shouldly;
+﻿using System;
+using Shouldly;
 using TrainingDay.MutationTesting;
 using Xunit;
 
@@ -9,12 +10,39 @@ namespace TrainingDay.Tests
         [Theory]
         [InlineData(60,46)]
         [InlineData(40,60)]
+        [InlineData(50,55)]
         public void Should_receive_damage_correctly(decimal damage,decimal expected)
         {
             var sut = new Donatello();
             sut.IaBataie(damage);
 
             sut.HealthPoints.ShouldBe(expected);
+        }
+
+        [Fact]
+        public void Should_throw_error_when_damage_is_negative()
+        {
+            var sut = new Donatello();
+
+            // Act
+            var ex = Should.Throw<Exception>(() => sut.IaBataie(-1));
+
+            // Assert
+            Assert.Equal("Something happened", ex.Message);
+        }
+
+        // fix this 
+        [Theory]
+        [InlineData(10)]
+        public void Should_return_correct_message(decimal damage)
+        {
+            var sut = new Donatello();
+
+            // Act
+            var response = sut.IaBataie(damage);
+
+            // Assert
+            response.ShouldBe("Successfully");
         }
     }
 }
